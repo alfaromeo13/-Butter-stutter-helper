946176c5330430bb39f5e6536a4abd74980dabfa
Jovan Vuković <60315689+alfaromeo13@users.noreply.github.com>
2023-02-22 02:54:54 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-Butter 
+# Butter
 
-Our team developed Butter, an AI-based integrated chatbot that provides an innovative speech-to-text solution for people who stutter. With this technology, we placed third in the OpenAI Whisper Hackathon. Butter uses advanced speech-to-text conversion to accurately transcribe live voice recordings, enabling individuals who stutter to communicate more effectively. In addition to transcribing messages, Butter can also answer personal questions about stuttering.
-Butter's state-of-the-art technology utilizes the Whisper API, created by LabLab AI, to intuitively translate speech into written form and omit any unintended interruptions in the flow of speech. Our goal is to empower people who stutter with improved accessibility to communication.
-By delivering accurate and reliable speech-to-text conversion, Butter helps individuals who stutter to communicate more effectively and confidently. Our technology is designed to provide a smoother communication experience and to help those who stutter express themselves more fluently.
-
-demo link -> https://lablab.ai/event/openai-whisper-hackathon/Boss/Butter
-
-link for hackathon -> https://lablab.ai/event/openai-whisper-hackathon 
+Speech-to-text solution for smoothing stuttering.
---
+++ main.py
@@ -0,0 +1,37 @@
+from auth import TELEGRAM_TOKEN
+import telebot
+from telebot import TeleBot
+from speech_to_text import get_text, get_answer
+
+
+def get_telegram_bot(token: str) -> None:
+    bot: TeleBot = telebot.TeleBot(token)
+
+    @bot.message_handler(commands=['start'])
+    def start_message(message):
+        intro_msg: str = "Hello! I'm Butter 🧈✨" \
+                         "\nHelping people who stutter is my job." \
+                         "\nSend me a voice message, and I'll transcribe it.🪄✨" \
+                         "\nYou can also text me any question you have about stuttering.🤓📚" \
+                         "\n" \
+                         "\n" \
+                         "To contact my creators: https://m.me/butterbot4u"
+        bot.send_message(message.chat.id, intro_msg)
+
+    @bot.message_handler(content_types=['voice'])
+    def voice_processing(message):
+        fid = bot.get_file(message.voice.file_id)
+        audio_file = bot.download_file(fid.file_path)
+        text: str = get_text(audio_file)
+        bot.reply_to(message, text)
+
+    @bot.message_handler(content_types=['text'])
+    def message_text_replier(message):
+        answer_msg: str = get_answer(message.text)
+        bot.reply_to(message, answer_msg)
+
+    bot.polling()
+
+
+if __name__ == '__main__':
+    get_telegram_bot(TELEGRAM_TOKEN)
